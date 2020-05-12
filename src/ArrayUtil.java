60048f58e9ae9f97a64174ef206832890b1b525a
Lidia Tkach <tkach.lidia61@gmail.com>
2020-05-12 22:59:03 +0300

    Homework ArrayUtil completed

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,17 +1,18 @@
-/**
- * ArrayUtil exercises.
- */
+import java.util.HashSet;
+
 public class ArrayUtil {
 
-    /**
-     * Finds the common elements between two arrays (String values).
-     *
-     * @param array1 first array
-     * @param array2 second array
-     * @return String array with common elements
-     */
     public static String[] findCommon(String[] array1, String[] array2) {
-        // TODO fill in code here
-        return new String[0];
+
+        HashSet<String> duplicates = new HashSet<>();
+
+        for (String s : array1) {
+            for (String value : array2) {
+                if (s.equals(value)) {
+                    duplicates.add(s);
+                }
+            }
+        }
+        return duplicates.toArray(new String[0]);
     }
 }
--- src/ArraysTest.java
+++ src/ArraysTest.java
@@ -5,11 +5,6 @@ import static org.testng.Assert.assertEquals;
 
 public class ArraysTest {
 
-    /**
-     * Test data for common array elements.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "ArrayUtil")
     public static Object[][] arrays() {
         return new Object[][]{
@@ -22,8 +17,17 @@ public class ArraysTest {
                         new String[]{"here", "is", "some", "text", ".", "text"},
                         new String[]{"some", "text", "above", "."},
                         new String[]{"some", "text", "."}
+                },
+                {
+                        new String[]{"hello", "bye", "thanks", "welcome", "hi", "bye"},
+                        new String[]{"hi", "hi", "hello", "hello", "hello"},
+                        new String[]{"hi", "hello"}
+                },
+                {
+                        new String[]{"red", "black", "white", "green"},
+                        new String[]{"blue", "yellow", "orange", "purple"},
+                        new String[]{}
                 }
-                // TODO add 2 more test data here
         };
     }
 
